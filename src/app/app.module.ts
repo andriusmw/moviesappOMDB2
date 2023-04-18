0f77caff72d071d5b527bcc6210f0523c58c40f1
andriusmw <andriusmw@yahoo.es>
2023-04-18 16:49:17 +0200

    search works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
